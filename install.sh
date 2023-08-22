ee9aaa3263383386b9182b5c3395d89a3da50ea1
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-08-22 21:51:48 +1000

    moving to cleaner prompt

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 zshrc() {
     echo "cloning zsh-autosuggestions"             
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    echo "clone powerlevel10k"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>.zshrc
     echo "Import zshrc"
     cat .zshrc > $HOME/.zshrc
 }
