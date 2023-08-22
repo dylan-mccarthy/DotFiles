b5bfdef22c8e68303727fc58829b74cf16a58045
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-08-22 21:37:00 +1000

    adding powerlevel10k install

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 zshrc() {
-    echo "==========================================================="
-    echo "             cloning zsh-autosuggestions                   "
-    echo "-----------------------------------------------------------"                    
+    echo "cloning zsh-autosuggestions"             
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    echo "==========================================================="
-    echo "                  Import zshrc                             "
-    echo "-----------------------------------------------------------"
+    echo "clone powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>.zshrc
+    echo "Import zshrc"
     cat .zshrc > $HOME/.zshrc
 }
 
